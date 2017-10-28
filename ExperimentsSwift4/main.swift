fdc75157b592b9c4e01a00c35250c4798d4f2558
Ajay Thakur <ajay.thakur@247-inc.com>
2017-10-28 15:55:42 -0700

    Initial Commit

---
+++ ExperimentsSwift4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExperimentsSwift4
+//
+//  Created by Ajay Thakur on 10/28/17.
+//  Copyright © 2017 Ajay Thakur. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
